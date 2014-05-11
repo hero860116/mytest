51c687bb2716415ac0aae1cf9cc00e1030f09470
lwl <hero_860116@hotmail.com>
2014-05-11 10:28:45 +0800

    commi

---
+++ spring_core_config/src/test/java/test/threads/CalculationThreadTest.java
@@ -0,0 +1,27 @@
+package test.threads;
+
+
+import org.junit.Test;
+
+/**
+ * User: weilin.li
+ * Date: 14-5-4
+ * Time: 下午6:56
+ */
+
+public class CalculationThreadTest {
+
+    @Test
+    public void test30Thread() {
+        Runnable runnable = new CalculationThread(1,1000000, System.currentTimeMillis());
+
+        runnable.run();
+    }
+
+    @Test
+    public void test1Thread() {
+        Runnable runnable = new CalculationThread(1,30000000, System.currentTimeMillis());
+
+        runnable.run();
+    }
+}
